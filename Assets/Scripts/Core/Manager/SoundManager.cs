541fa9cee5cddbcc54e77741785b296e492829d1
시원 <96858320+NearthYou@users.noreply.github.com>
2024-07-03 22:10:11 +0900

    [Add] DOTween 설치

--- Assets/Scripts/Core/Manager/SoundManager.cs
+++ Assets/Scripts/Core/Manager/SoundManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using DG.Tweening;
 using Unity.VisualScripting;
 using UnityEditor.PackageManager.UI;
 using UnityEngine;
