b2c605e0fbaec3fa02d4394156291aeb98b39d8d
rbase0218 <rbase@naver.com>
2024-07-09 03:53:46 +0900

    UIGauge GO

---
+++ Assets/Scripts/UI/Elements/UIGauge.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class UIGauge : MonoBehaviour
+{
+    public static UIGauge instance = null;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+    public Image _gauge;
+    public UnityEvent onEndGauge;
+
+    private float timer = .0f;
+    private float maxTime = .0f;
+    
+    public void ResetGauge()
+    {
+        _gauge.fillAmount = 1f;
+    }
+
+    public void SetTime(float time = 1f)
+    {
+        maxTime = time;
+    }
+
+    public void Play()
+    {
+        StartCoroutine("StartGauge");
+    }
+
+    private IEnumerator StartGauge()
+    {
+        var startValue = _gauge.fillAmount;
+        timer = .0f;
+        
+        while (timer < maxTime)
+        {
+            _gauge.fillAmount = Mathf.Lerp(startValue, 0f, timer / maxTime);
+            yield return null;
+            timer += Time.deltaTime;
+        }
+        
+        onEndGauge?.Invoke();
+    }
+    
+}
