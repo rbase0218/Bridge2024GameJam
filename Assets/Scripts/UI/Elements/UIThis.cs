7f34951cc026063cb07ceb1891bd4338fb41199b
rbase0218 <rbase@naver.com>
2024-07-09 00:42:34 +0900

    gg

--- Assets/Scripts/UI/Elements/UIThis.cs
+++ Assets/Scripts/UI/Elements/UIThis.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class UIThis : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public List<GameObject> _objects;
+    
+    // Frame 8
+    public UIJobGroup _frame8;
+    // Frame 9
+    public UIJobGroup _frame9Job;
+    public UICardElement _frame9Card;
+    // Frame 11
+    public UIJobGroup _frame11;
 
-    // Update is called once per frame
-    void Update()
+    public void OpenFrame(int count)
     {
+        foreach(var obj in _objects)
+            obj.SetActive(false);
         
+        if(count == 8)
+            _objects[0].SetActive(true);
+        else if(count == 9)
+            _objects[1].SetActive(true);
+        else if(count == 11)
+            _objects[2].SetActive(true);
     }
 }
