d6fec9bbc4fa7d35f1c267fded3538982078e7a6
rbase0218 <rbase@naver.com>
2024-07-28 15:51:27 +0900

    Remove : 재작업 요소에 포함되는 Roles 삭제 및 Project Init

--- Assets/Scripts/Utils/ITimerControl.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface ITimerControl
-{
-    public void PauseTimer();
-    public void RestartTimer();
-}
