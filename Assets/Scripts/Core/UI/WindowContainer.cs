01ccd37d21a6636bcac3285acb0fdf73e61335c4
rbase0218 <rbase@naver.com>
2024-07-28 21:28:36 +0900

    Feat : 모든 Window를 관리하는 Container 클래스 구현

---
+++ Assets/Scripts/Core/UI/WindowContainer.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowContainer : Dictionary<Type, UIWindow>
+{
+    public bool AddWindow(Type type, UIWindow window)
+    {
+        return TryAdd(type, window);
+    }
+
+    public T GetWindow<T>() where T : UIWindow
+    {
+        return this[typeof(T)] as T;
+    }
+
+    public void RemoveWindow(Type type)
+    {
+        this.Remove(type);
+    }
+
+    public void RemoveAllWindow()
+    {
+        if (Count <= 0) return;
+        
+        foreach (var window in this)
+        {
+            var key = window.GetType();
+            RemoveWindow(key);
+        }
+    }
+    
+    public bool ContainWindow<T>()
+    {
+        return ContainsKey(typeof(T));
+    }
+}
