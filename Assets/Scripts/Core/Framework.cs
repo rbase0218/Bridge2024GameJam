f3552f43b90ff9c42135573f22a01cd50e559a1d
rbase0218 <rbase@naver.com>
2024-07-28 21:56:26 +0900

    Fix : 초기 Framework CS 파일 추가

---
+++ Assets/Scripts/Core/Framework.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Framework : MonoBehaviour
+{
+    private void Awake()
+    {
+        // Base Start
+    }
+}
