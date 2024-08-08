cde7c2ee9bdb388c03f70d7a6e31de5f6da2715f
rbase0218 <rbase@naver.com>
2024-08-08 17:53:38 +0900

    Feat : UIIntroText Window에 대한 퍼블리싱 처리 작업

---
+++ Assets/Scripts/Contents/UI/Window/UIIntroText.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIIntroText : UIWindow
+{
+    public enum NameTag
+    {
+        NameTagContainer
+    }
+
+    public enum Texts
+    {
+        Text1,
+        Text2
+    }
+    
+    protected override bool Init()
+    {
+        if (!base.Init())
+            return false;
+        
+        Bind<UINameTagContainer>(typeof(NameTag));
+        Bind<UITextSegment>(typeof(Texts));
+
+        return true;
+    }
+    
+    protected override bool EnterWindow()
+    {
+        // Window가 Open 되기 전에 해당 처리를 진행한다.
+        
+        return true;
+    }
+
+    public void SetInfo(string name, string text1, string text2)
+    {
+        Get<UINameTagContainer>((int)NameTag.NameTagContainer).SetText(name);
+        Get<UITextSegment>((int)Texts.Text1).SetText(text1);
+        Get<UITextSegment>((int)Texts.Text2).SetText(text2);
+    }
+}
