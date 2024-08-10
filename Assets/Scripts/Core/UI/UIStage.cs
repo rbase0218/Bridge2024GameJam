382c8bea815a28bc8edeb4bba4df24d817c25261
rbase0218 <rbase@naver.com>
2024-08-10 20:21:47 +0900

    Fix : 복수의 Screen들을 관리하는 UIStage 클래스 구현

---
+++ Assets/Scripts/Core/UI/UIStage.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class UIStage : UIBase
+{
+    // Stage는 다음 Stage에 대한 정보를 가지고 있다.
+    protected UIStage nextStage;
+
+    public Action OnNextStage;
+    
+    protected override bool Init()
+    {
+        if (!base.Init())
+            return false;
+
+        return true;
+    }
+
+    public abstract void OnFirstScreen();
+}
