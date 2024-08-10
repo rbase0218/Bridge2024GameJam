19c04b25e27d4245c9c02be713696ba214c8fe9a
rbase0218 <rbase@naver.com>
2024-08-10 20:22:01 +0900

    Fix : 복수의 UIStage들을 관리하는 UIStageManager 구현

---
+++ Assets/Scripts/Core/UI/UIStageManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIStageManager : UIBase
+{
+    public enum Stages
+    {
+        IntroStage,
+        JobCheckStage,
+        QuestionStage,
+        VotingStage,
+        VoteResultStage,
+        FinalResultStage
+    }
+
+    protected override bool Init()
+    {
+        if (!base.Init())
+            return false;
+
+        Bind<UIStage>(typeof(Stages));
+        return true;
+    }
+
+    public void Play()
+    {
+    }
+
+    private void OnFirstStage()
+    {
+        Get<UI_IntroStage>((int)Stages.IntroStage).OnFirstScreen();
+    }
+}
