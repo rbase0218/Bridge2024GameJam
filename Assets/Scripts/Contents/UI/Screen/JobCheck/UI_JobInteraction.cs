278ce1679c0493084b0e88017fef0b677cb2563c
rbase0218 <rbase@naver.com>
2024-08-12 00:17:42 +0900

    Fix : IntroStage / JobCheckStage - JobInteraction까지 구현

--- Assets/Scripts/Contents/UI/Screen/JobCheck/UI_JobInteraction.cs
+++ Assets/Scripts/Contents/UI/Screen/JobCheck/UI_JobInteraction.cs
@@ -9,21 +9,6 @@ public class UI_JobInteraction : UIScreen
         Board_A,
         Board_B
     }
-
-    private enum JobInfo 
-    {
-        NameTagContainer
-    }
-
-    private enum Card
-    {
-        CardContainer
-    }
-
-    private enum Select
-    {
-        SelectContainer
-    }
     
     protected override bool Init()
     {
@@ -37,6 +22,21 @@ public class UI_JobInteraction : UIScreen
     
     protected override bool EnterWindow()
     {
+        // 1. 현재 진행중인 유저의 정보를 가져온다.
+        var user = Managers.Game._currentUser;
+        
+        // 2. 유저의 정보에 따라서 Board A와 Board B를 활성화한다.
+        if (user.jobType == EJobType.Actor || user.jobType == EJobType.VIP)
+        {
+            GetObject((int)Boards.Board_A).SetActive(true);
+            GetObject((int)Boards.Board_B).SetActive(false);
+        }
+        else
+        {
+            GetObject((int)Boards.Board_B).SetActive(true);
+            GetObject((int)Boards.Board_A).SetActive(false);
+        }
+
         return true;
     }
 }
