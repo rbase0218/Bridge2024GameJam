feea58302deab8b69eec5d0a68ec90ddce599a63
rbase0218 <rbase@naver.com>
2024-08-12 03:48:48 +0900

    Feat : Button Select 기능 완성 및 Submit 버튼 클릭 시 반환 기능 추가

--- Assets/Scripts/Contents/UI/Screen/JobCheck/UI_JobInteraction.cs
+++ Assets/Scripts/Contents/UI/Screen/JobCheck/UI_JobInteraction.cs
@@ -9,6 +9,12 @@ public class UI_JobInteraction : UIScreen
         Board_A,
         Board_B
     }
+
+    #region # [ Board B Component ] #
+    
+    private UIPlayerSelector _playerSelector;
+    
+    #endregion
     
     protected override bool Init()
     {
@@ -16,6 +22,8 @@ public class UI_JobInteraction : UIScreen
             return false;
         
         BindObject(typeof(Boards));
+        _playerSelector = Utils.FindChild<UIPlayerSelector>(gameObject, "PlayerSelect", true);
+        _playerSelector.Binding();
 
         return true;
     }
@@ -26,15 +34,16 @@ public class UI_JobInteraction : UIScreen
         var user = Managers.Game._currentUser;
         
         // 2. 유저의 정보에 따라서 Board A와 Board B를 활성화한다.
-        if (user.jobType == EJobType.Actor || user.jobType == EJobType.VIP)
-        {
-            GetObject((int)Boards.Board_A).SetActive(true);
-            GetObject((int)Boards.Board_B).SetActive(false);
-        }
-        else
+        // if (user.jobType == EJobType.Actor || user.jobType == EJobType.VIP)
+        // {
+        //     GetObject((int)Boards.Board_A).SetActive(true);
+        //     GetObject((int)Boards.Board_B).SetActive(false);
+        // }
+        // else
         {
             GetObject((int)Boards.Board_B).SetActive(true);
             GetObject((int)Boards.Board_A).SetActive(false);
+            
         }
 
         return true;
