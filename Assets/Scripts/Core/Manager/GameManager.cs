fb2a1f9ce4538be068d6fd059fe25ba13a363daf
rbase0218 <rbase@naver.com>
2024-08-11 21:43:15 +0900

    Fix : GameManager에 User Info 데이터 추가

--- Assets/Scripts/Core/Manager/GameManager.cs
+++ Assets/Scripts/Core/Manager/GameManager.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private List<UserInfo> _userList = new List<UserInfo>();
+
+    // 현재 직업이 암살자인 유저를 찾아둔다.
+    private UserInfo _assUser;
     
+    // 현재 인질로 지정된 유저를 찾아둔다.
+    private UserInfo _hostageUser;
+    
+    
+    public void AddUser(params UserInfo[] users)
+    {
+        _userList.AddRange(users);
+    }
 }
