8276034e8936ecc6ca25be84ba81a4499cf0dc93
rbase0218 <rbase@naver.com>
2024-08-19 18:16:35 +0900

    Fix : Modal의 기본 기능 구현

--- Assets/Scripts/Contents/UI/Title/UIGameSetting.cs
+++ Assets/Scripts/Contents/UI/Title/UIGameSetting.cs
@@ -1,19 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class UIGameSetting : UIWindow
 {
+    private enum Buttons
+    {
+        SaveButton,
+        ExitButton
+    }
+    
     protected override bool Init()
     {
         if (!base.Init())
             return false;
 
+        BindButton(typeof(Buttons));
 
         return true;
     }
     protected override bool EnterWindow()
     {
         return true;
+    }
+    
+    public void OnClickButtons(UnityAction yesButton, UnityAction noButton = null)
+    {
+        GetButton((int)Buttons.SaveButton).onClick.RemoveAllListeners();
+        GetButton((int)Buttons.ExitButton).onClick.RemoveAllListeners();
+        
+        GetButton((int)Buttons.SaveButton).onClick.AddListener(yesButton);
+        if (noButton == null)
+        {
+            GetButton((int)Buttons.ExitButton).onClick.AddListener(() =>
+            {
+                Managers.UI.CloseWindow();
+            });
+        }
+        else
+            GetButton((int)Buttons.ExitButton).onClick.AddListener(noButton);
     }
 }
