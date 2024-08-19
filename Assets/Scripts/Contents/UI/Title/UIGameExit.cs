977ccac0bb8fae7a75ab2b662cdb8da8d46d5429
rbase0218 <rbase@naver.com>
2024-08-19 18:10:13 +0900

    Fix : GameExit 구현

--- Assets/Scripts/Contents/UI/Title/UIGameExit.cs
+++ Assets/Scripts/Contents/UI/Title/UIGameExit.cs
@@ -1,19 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class UIGameExit : UIWindow
 {
+    private enum Buttons
+    {
+        YesButton,
+        NoButton
+    }
+    
     protected override bool Init()
     {
         if (!base.Init())
             return false;
 
+        BindButton(typeof(Buttons));
 
         return true;
     }
     protected override bool EnterWindow()
     {
         return true;
+    }
+    
+    public void OnClickButtons(UnityAction yesButton, UnityAction noButton = null)
+    {
+        GetButton((int)Buttons.YesButton).onClick.RemoveAllListeners();
+        GetButton((int)Buttons.NoButton).onClick.RemoveAllListeners();
+        
+        GetButton((int)Buttons.YesButton).onClick.AddListener(yesButton);
+        if (noButton == null)
+        {
+            GetButton((int)Buttons.NoButton).onClick.AddListener(() =>
+            {
+                Managers.UI.CloseWindow();
+            });
+        }
+        else
+            GetButton((int)Buttons.NoButton).onClick.AddListener(noButton);
     }
 }
