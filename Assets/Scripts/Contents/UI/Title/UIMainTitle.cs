934a6c19002d6a787db2d0418d9231f39b058e4e
rbase0218 <rbase@naver.com>
2024-08-19 18:10:13 +0900

    Fix : GameExit 구현

--- Assets/Scripts/Contents/UI/Title/UIMainTitle.cs
+++ Assets/Scripts/Contents/UI/Title/UIMainTitle.cs
@@ -4,16 +4,74 @@ using UnityEngine;
 
 public class UIMainTitle : UIWindow
 {
+    private enum Buttons
+    {
+        BackButton,
+        HomeButton,
+        ManualButton,
+        StartButton,
+        SettingButton,
+        ExitButton
+    }
+    
     protected override bool Init()
     {
+        // Main Title은 항상 UI를 보여주고 있는 상태를 가진다.
+        alwaysOpen = true;
+        
         if (!base.Init())
             return false;
         
+        BindButton(typeof(Buttons));
+        
+        GetButton((int)Buttons.StartButton).onClick.AddListener(OnClickStartButton);
+        GetButton((int)Buttons.SettingButton).onClick.AddListener(OnClickSettingButton);
+        GetButton((int)Buttons.ExitButton).onClick.AddListener(OnClickExitButton);
+        
+        GetButton((int)Buttons.BackButton).onClick.AddListener(OnClickBackButton);
+        GetButton((int)Buttons.HomeButton).onClick.AddListener(OnClickHomeButton);
+        GetButton((int)Buttons.ManualButton).onClick.AddListener(OnClickManualButton);
         
         return true;
     }
     protected override bool EnterWindow()
     {
         return true;
+    }
+    
+    // 게임 실행에 직접적으로 영향을 끼치는 Button들
+    private void OnClickStartButton()
+    {
+        Managers.UI.ShowWindow<UIStartProcess>();
+    }
+    
+    private void OnClickSettingButton()
+    {
+        Managers.UI.ShowWindow<UIGameSetting>();
+    }
+    
+    private void OnClickExitButton()
+    {
+        var exit = Managers.UI.ShowWindow<UIGameExit>();
+        exit.OnClickButtons(() =>
+        {
+            Debug.Log("게임 종료");
+        });
+    }
+    
+    // 상단 부분에 있는 Button들
+    private void OnClickManualButton()
+    {
+        Managers.UI.ShowWindow<UIManual>();   
+    }
+    
+    private void OnClickHomeButton()
+    {
+        Managers.UI.ShowWindow<UIReturnMain>();
+    }
+    
+    private void OnClickBackButton()
+    {
+        Managers.UI.CloseWindow();
     }
 }
