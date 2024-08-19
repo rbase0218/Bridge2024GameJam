7cbc3f2afd4b5a3a375a39d95ad4c1576352df2c
rbase0218 <rbase@naver.com>
2024-08-19 14:10:40 +0900

    Fix : Voting Stage Script 연결 및 Screen Swap

--- Assets/Scripts/Contents/UI/Screen/Voting/UI_NextPlayerV.cs
+++ Assets/Scripts/Contents/UI/Screen/Voting/UI_NextPlayerV.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 
 public class UI_NextPlayerV : UIScreen
 {
+    private enum Texts
+    {
+        NameA,
+        NameB
+    }
+
+    private enum Buttons
+    {
+        NextButton
+    }
+    
     protected override bool Init()
     {
         if (!base.Init())
             return false;
 
+        BindText(typeof(Texts));
+        BindButton(typeof(Buttons));
+        
         return true;
     }
     
     protected override bool EnterWindow()
     {
+        if(UseAutoNextScreen)
+            BindNextScreen<UI_NextPlayerV>();
+        
         return true;
     }
 }
