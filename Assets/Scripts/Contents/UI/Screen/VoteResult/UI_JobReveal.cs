8561831500aceceb31933d4e52433ffb2d1dd4a5
rbase0218 <rbase@naver.com>
2024-08-20 18:41:34 +0900

    Fix : Data 추가 및 UI Components Bind

--- Assets/Scripts/Contents/UI/Screen/VoteResult/UI_JobReveal.cs
+++ Assets/Scripts/Contents/UI/Screen/VoteResult/UI_JobReveal.cs
@@ -4,16 +4,44 @@ using UnityEngine;
 
 public class UI_JobReveal : UIScreen
 {
+    private enum Texts
+    {
+        FirstText,
+        NameText,
+        ThirdText
+    }
+
+    private enum Buttons
+    {
+        NextButton
+    }
+
+    private enum Images
+    {
+        Picture
+    }
+    
     protected override bool Init()
     {
         if (!base.Init())
             return false;
 
+        BindText(typeof(Texts));
+        BindImage(typeof(Images));
+        BindButton(typeof(Buttons));
+        
         return true;
     }
     
     protected override bool EnterWindow()
     {
+        var voteUser = Managers.Game.voteUser;
+        var voteUserName = voteUser.userName;
+        var voteUserPicture = Managers.Data.GetFrameSprite(voteUser.jobType);
+        
+        GetText((int)Texts.FirstText).SetText($"{voteUserName}은(는)");
+        GetText((int)Texts.ThirdText).SetText("입니다.");
+        
         if(UseAutoNextScreen)
             BindNextScreen<UI_LastChance>();
         
--- Assets/Scripts/Contents/UI/Screen/VoteResult/UI_VoteResult.cs
+++ Assets/Scripts/Contents/UI/Screen/VoteResult/UI_VoteResult.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class UI_VoteResult : UIScreen
 {
+    private enum Texts
+    {
+        SecondText
+    }
+    
     protected override bool Init()
     {
         if (!base.Init())
             return false;
-
+        
+        BindText(typeof(Texts));
+        
         return true;
     }
     
     protected override bool EnterWindow()
     {
+        var voteUserName = Managers.Game.voteUser.userName;
+        
+        GetText((int)Texts.SecondText).SetText(
+            $"{voteUserName}님이 암살자로\n지목되었습니다.");
+        
         if(UseAutoNextScreen)
             BindNextScreen<UI_JobReveal>();
-        
         return true;
     }
 }
