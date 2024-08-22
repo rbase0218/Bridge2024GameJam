c622c5ba0816bb4284a666590dbaf104fde5712d
시원 <96858320+NearthYou@users.noreply.github.com>
2024-08-23 03:50:50 +0900

    Fix : Same VoteCount

--- Assets/Scripts/Contents/UI/Screen/VoteResult/UI_Switcher01VR.cs
+++ Assets/Scripts/Contents/UI/Screen/VoteResult/UI_Switcher01VR.cs
@@ -20,6 +20,27 @@ public class UI_Switcher01VR : UIScreen
         if (voteUser == null)
             voteUser = Managers.Game.currentUser;
         
+        int voteCount = 0;
+        foreach (var vote in Managers.Game._voteList)
+        {
+            if(voteCount < vote.Key)
+                voteCount = vote.Key;
+            else if(voteCount == vote.Key)
+            {
+                // 동표 발생
+                // 동표 나올 경우 결과 창에서 문구 띄운 후 다시 Vote 화면으로 전환
+            }
+        }
+        
+        // 여기서 암살자가 인질을 모두 잡았을 경우 게임 종료 분기 필요
+        if (Managers.Game._hostageList.Count == Managers.Game._userList.Count)
+        {
+            Managers.Game.voteUser = Managers.Game._userList.Find(x => x.jobType == EJobType.Assassin);
+            var uiLastChanceResult = BindNextScreen<UI_LastChanceResult>();
+            uiLastChanceResult.SetInfo(true);
+            return true;
+        }
+        
         Managers.Game.voteUser = voteUser;
         Managers.Game._voteList.Clear();
         
