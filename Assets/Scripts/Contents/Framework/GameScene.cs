a16b0f6fc970e5197138eec73c3fb7b310516fb9
rbase0218 <rbase@naver.com>
2024-08-23 19:47:31 +0900

    Fix : 디버그 주석 제거

--- Assets/Scripts/Contents/Framework/GameScene.cs
+++ Assets/Scripts/Contents/Framework/GameScene.cs
@@ -14,11 +14,11 @@ public sealed class GameScene : Framework
     private void Update()
     {
 #if !UNITY_EDITOR
-        // if (isPlay == false)
-        // {
-        //     isPlay = true;
-        //     Managers.UI.ShowWindow<UI_Intro01>();
-        // }
+        if (isPlay == false)
+        {
+            isPlay = true;
+            Managers.UI.ShowWindow<UI_Intro01>();
+        }
 #else
         if(Input.GetKeyDown(KeyCode.Space))
         {
