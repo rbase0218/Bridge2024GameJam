ec681b644d4c3fb36120a8b40988a58ab3400ec9
Siwon <fhrhdls@naver.com>
2025-06-19 22:58:04 +0900

    Feat: Add Emergency Button

--- Assets/Scripts/Contents/UI/Screen/0_Common/UI_InGameSetting.cs
+++ Assets/Scripts/Contents/UI/Screen/0_Common/UI_InGameSetting.cs
@@ -61,6 +61,6 @@ public class UI_InGameSetting : UIWindow
     private void OnClickEmergencyButton()
     {
         Managers.Sound.PlaySFX("Click");
-        //Managers.UI.ShowWindow<UI_Emergency>();
+        Managers.UI.ShowWindow<UI_Switcher01_Last>();
     }
 }
---
+++ Assets/Scripts/Contents/UI/Screen/Voting/UI_Switcher01_Last.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Switcher01_Last : UIScreen
+{
+    protected override bool Init()
+    {
+        if (!base.Init())
+            return false;
+
+        return true;
+    }
+    
+    protected override bool EnterWindow()
+    {
+        if(UseAutoNextScreen)
+            BindNextScreen<UI_Introduce>();
+        
+        // 플레이어 데이터를 투표자로 변경한다.
+        Managers.Game.SetContext(PlayersDataContext.DataContextType.Voter);
+        return true;
+    }
+}
