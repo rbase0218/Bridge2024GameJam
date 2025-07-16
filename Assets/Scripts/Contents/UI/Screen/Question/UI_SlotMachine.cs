4c027622884710ba558db8b6b27c454222a89b55
rbase0218 <rbase@naver.com>
2025-07-16 23:28:58 +0900

    Fix : SlotMachine 기능 구현

--- Assets/Scripts/Contents/UI/Screen/Question/UI_SlotMachine.cs
+++ Assets/Scripts/Contents/UI/Screen/Question/UI_SlotMachine.cs
@@ -8,12 +8,20 @@ public class UI_SlotMachine : UIScreen
     {
         NextButton
     }
+
+    private enum GameObjects
+    {
+        SlotObject,
+    }
+
+    private GameObject _slotObject;
     protected override bool Init()
     {
         if (!base.Init())
             return false;
         
         BindButton(typeof(Buttons));
+        BindObject(typeof(GameObjects));
         
         GetButton((int)Buttons.NextButton).onClick.AddListener(OnClickNextButton);
         return true;
@@ -21,14 +29,20 @@ public class UI_SlotMachine : UIScreen
 
     protected override bool EnterWindow()
     {
+        onOpen?.RemoveAllListeners();
+        
         var maxCount = Managers.Game.GetQuestionLogCount();
-        var randNum = Random.Range(1, maxCount);
+        var resultNumber = Random.Range(1, maxCount);
+
+        // 위치 초기화
+        _slotObject = GetObject((int)GameObjects.SlotObject);
+        _slotObject.transform.localPosition = Vector3.zero;
         
         // 랜덤 수를 미리 정함.
-        Managers.Game.SetRandQuestionCount(randNum);
-        Debug.Log("다음 질문 개수 : " + randNum);
-        
+        Managers.Game.SetRandQuestionCount(resultNumber);
         // 이후로 SlotMachine을 만들면 됨.
+        
+        onOpen?.AddListener(() => StartCoroutine(StartSlotCoroutine()));
         
         return true;
     }
@@ -37,4 +51,20 @@ public class UI_SlotMachine : UIScreen
     {
         OnNextScreen<UI_TextConfirm01>();
     }
+
+    private IEnumerator StartSlotCoroutine()
+    {
+        Debug.Log("Coroutine 동작");
+        for (int i = 0; i < (20 * 8) + (20 * 1) ; ++i)
+        {
+            _slotObject.transform.localPosition += Vector3.up * 31.25f;
+            if (_slotObject.transform.localPosition.y > 1250f)
+            {
+                _slotObject.transform.localPosition = Vector3.zero;
+            }
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+    
+    
 }
