a15d517ac24e1f739b487148545fc32a434241c5
rbase0218 <rbase@naver.com>
2025-07-17 20:44:55 +0900

    Fix : SlotMachine에서 잘못 표기된 데이터 수정

--- Assets/Scripts/Contents/UI/Screen/Question/UI_SlotMachine.cs
+++ Assets/Scripts/Contents/UI/Screen/Question/UI_SlotMachine.cs
@@ -15,6 +15,8 @@ public class UI_SlotMachine : UIScreen
     }
 
     private GameObject _slotObject;
+    private int _maxCount = 0;
+    
     protected override bool Init()
     {
         if (!base.Init())
@@ -31,16 +33,16 @@ public class UI_SlotMachine : UIScreen
     {
         onOpen?.RemoveAllListeners();
         
-        var maxCount = Managers.Game.GetQuestionLogCount();
-        var resultNumber = Random.Range(1, maxCount);
+        _maxCount = Managers.Game.QuestionManager.GetLogCount();
+        var resultNumber = Random.Range(1, _maxCount);
 
         // 위치 초기화
         _slotObject = GetObject((int)GameObjects.SlotObject);
         _slotObject.transform.localPosition = Vector3.zero;
         
         // 랜덤 수를 미리 정함.
-        Managers.Game.SetRandQuestionCount(resultNumber);
-        // 이후로 SlotMachine을 만들면 됨.
+        Managers.Game.QuestionManager.SelectRandQuestion(resultNumber);
+        Debug.Log("질문 개수 : " + resultNumber);
         
         onOpen?.AddListener(() => StartCoroutine(StartSlotCoroutine()));
         
@@ -55,16 +57,13 @@ public class UI_SlotMachine : UIScreen
     private IEnumerator StartSlotCoroutine()
     {
         Debug.Log("Coroutine 동작");
-        for (int i = 0; i < (20 * 8) + (20 * 1) ; ++i)
+        for (int i = 0; i < (20 * 8) + (20 * _maxCount) ; ++i)
         {
             _slotObject.transform.localPosition += Vector3.up * 31.25f;
             if (_slotObject.transform.localPosition.y > 1250f)
-            {
                 _slotObject.transform.localPosition = Vector3.zero;
-            }
+            
             yield return new WaitForSeconds(0.01f);
         }
     }
-    
-    
 }
